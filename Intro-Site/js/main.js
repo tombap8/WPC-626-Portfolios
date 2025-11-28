28ac5c048e2d755166dcff5a93e83545ae1ff690
Tom B. Parker <tombap8@hotmail.com>
2025-11-28 10:44:38 +0900

    ss

--- Intro-Site/js/main.js
+++ Intro-Site/js/main.js
@@ -1,8 +1,60 @@
 // 발표 메인 페이지 JS - main.js
 
-$(function () {
-  // jQB /////////////////
+// JSON 데이터 로드 및 DOM 생성
+function loadDevelopers() {
+  $.getJSON('./Intro-Site/data/developers.json', function(data) {
+    const $sbox = $('.sbox.s1');
+    $sbox.empty(); // 기존 내용 삭제
+    
+    data.forEach(function(developer) {
+      // li 요소 생성
+      const $li = $('<li></li>');
+      
+      // 이름 링크 생성
+      const $nameLink = $('<a href="#" class="kw"></a>').text(developer.name);
+      
+      // cbox 생성
+      const $cbox = $('<div class="cbox"></div>');
+      if (developer.cssClass) {
+        $cbox.addClass(developer.cssClass);
+      }
+      
+      // cbox 내부 구조
+      $cbox.append('<div></div>');
+      
+      const $contentDiv = $('<div></div>');
+      
+      // 제목
+      const $title = $('<h2 class="tit"></h2>').text(developer.project);
+      
+      // 이미지
+      const $img = $('<img>').attr({
+        src: developer.image,
+        alt: developer.altText
+      });
+      
+      // 링크들
+      const $desc = $('<p class="desc"></p>');
+      developer.links.forEach(function(link) {
+        const $link = $('<a></a>').attr('href', link.url).text(link.type);
+        $desc.append($link);
+      });
+      
+      // 조립
+      $contentDiv.append($title).append($img).append($desc);
+      $cbox.append($contentDiv);
+      
+      $li.append($nameLink).append($cbox);
+      $sbox.append($li);
+    });
+    
+    // 데이터 로드 후 이벤트 바인딩
+    bindEvents();
+  });
+}
 
+// 이벤트 바인딩 함수
+function bindEvents() {
   // 이름원을 클릭할때 상세 프로젝트 열기
   $(".sbox li").click(function () {
     $(this).addClass("on").siblings().removeClass("on");
@@ -34,6 +86,13 @@ $(function () {
 
     window.open().location.href = tgsrc;
   }); //// click ///////
+}
+
+$(function () {
+  // jQB /////////////////
+  
+  // JSON 데이터 로드
+  loadDevelopers();
 
   $("#dpbox>span").click(function () {
     // 숨기기
